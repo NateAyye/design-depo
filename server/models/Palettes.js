ef1b4d718986460b79c0da6081e184cf25daf33d
Beatriz Sandoval <beasaddress@gmail.com>
2023-08-24 19:39:16 -0700

    added a model for palettes

---
+++ server/models/Palettes.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const paletteSchema = new Schema(
+    {
+        paletteName: {
+            type: String,
+            required: true,
+            minlength: 5,
+            maxlength: 60,
+        },
+            createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => {
+                return timestamp.toISOString();
+            },
+        },
+        color1: {
+            type: String,
+            required: true,
+            match: /^#?([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/,
+        },
+        color2: {
+            type: String,
+            required: true,
+            match: /^#?([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/,
+        },
+        color3: {
+            type: String,
+            required: true,
+            match: /^#?([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/,
+        },
+        color4: {
+            type: String,
+            required: true,
+            match: /^#?([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/,
+        },
+        color5: {
+            type: String,
+            required: true,
+            match: /^#?([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/,
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+          },
+          id: false,  
+    }
+);
+
+const Palettes = model('palettes', paletteSchema);
+module.exports = Palettes;
