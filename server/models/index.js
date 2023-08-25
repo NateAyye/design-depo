b8bccd7fe87f062b5483c656958698fca87ceb9a
Beatriz Sandoval <beasaddress@gmail.com>
2023-08-24 21:28:48 -0700

    added index.js to models folder

---
+++ server/models/index.js
@@ -0,0 +1,8 @@
+const User = require('./User');
+const Palettes = require('./Palettes');
+const Gradients = require('./Gradients');
+const Projects = require('./Projects');
+const Fonts = require('./Fonts');
+const Colors = require('./Colors');
+
+module.exports = { User, Palettes, Gradients, Projects, Fonts, Colors };
