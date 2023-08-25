378d543a4853f7c29577b0c6632b10d856bf15db
[Ghias Naser] <[ghiasnaser@gmail.com]>
2023-08-24 20:26:14 -0700

    adding the schemas files

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,17 @@
+const { Tech, Matchup } = require('../models');
+
+const resolvers = {
+  Query: {
+    : async () => {
+     
+    },
+    : async (parent, { _id }) => {
+      
+    },
+  },
+  Mutation: {
+    
+  },
+};
+
+module.exports = resolvers;
