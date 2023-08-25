acd4b3f28544741467b88caf33a31e3c6fbaa6da
Beatriz Sandoval <beasaddress@gmail.com>
2023-08-24 19:18:30 -0700

    updated references to other models and changed the name of the profile model to User

---
+++ server/models/User.js
@@ -0,0 +1,72 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+    },
+    palettes: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'palettes',
+        },
+    ],
+
+    gradients: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'gradients',
+        },
+    ],
+    colors: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'colors',
+        }
+    ],
+    fonts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'colors', 
+        }
+    ],
+    projects: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'colors', 
+        }
+    ]
+
+});
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+
+  userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
+  
+  const User = model('User', userSchema);
+  
+  module.exports = User;
+  
