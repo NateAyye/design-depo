6e2f1e9b671b1c27ed65ec2aad3404840b9f3192
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-08-29 16:36:18 -0700

    Added a CREATE_PALETTE mutation and a QUERY_ALL_PALETTES query that aligned with the server

--- client/src/lib/mutations.js
+++ client/src/lib/mutations.js
@@ -11,6 +11,33 @@ mutation createColor($hexCode: String!, $name: String!, $userId: ID!) {
   }
 }
 `;
+export const CREATE_PALETTE = gql`
+mutation CreatePalette($userId: ID!, $paletteName: String!, $color1: String!, $color2: String!, $color3: String!, $color4: String!, $color5: String!) {
+  createPalette(userId: $userId, paletteName: $paletteName, color1: $color1, color2: $color2, color3: $color3, color4: $color4, color5: $color5) {
+    _id
+    userId
+    paletteName
+    createdAt
+    colors
+    color1
+    color2
+    color3
+    color4
+    color5
+  }
+}
+`;
+export const DELETE_PALETTE = gql`
+mutation Mutation($paletteId: ID!) {
+    deletePalette(id: $paletteId) {
+      _id
+      userId
+      paletteName
+      createdAt
+      colors
+    }
+  }
+`;
 
 export const CREATE_USER = gql`
 mutation CreateUser($name: String!, $email: String!, $password: String!) {
