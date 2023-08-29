39ad520b92f9661f4297db4bdcc3f94a3d3b65ca
[Ghias Naser] <[ghiasnaser@gmail.com]>
2023-08-29 13:17:30 -0700

    adding the project mutations and Seeds

--- server/seed/seed.js
+++ server/seed/seed.js
@@ -9,20 +9,23 @@ const Fonts =require('../models/Fonts');
 const fontSeeds = require('./fontSeeds.json');
 const Palette = require('../models/Palettes');
 const paletteSeeds = require('./paletteSeeds.json');
+const Project = require ('../models/Projects');
+const projectSeeds = require('./projectSeeds.json');
 
 db.once('open', async () => {
   try {
-    await User.deleteMany({});
-    await User.create(userSeeds);
-    await Color.deleteMany({});
-    await Color.create(colorSeeds);
-    await Gradient.deleteMany({});
-    await Gradient.create(gradientSeeds);
-    await Fonts.deleteMany({});
-    await Fonts.create(fontSeeds);
-    await Palette.deleteMany({});
-    await Palette.create(paletteSeeds);
-
+    // await User.deleteMany({});
+    // await User.create(userSeeds);
+    // await Color.deleteMany({});
+    // await Color.create(colorSeeds);
+    // await Gradient.deleteMany({});
+    // await Gradient.create(gradientSeeds);
+    // await Fonts.deleteMany({});
+    // await Fonts.create(fontSeeds);
+    // await Palette.deleteMany({});
+    // await Palette.create(paletteSeeds);
+     await Project.deleteMany({});
+     await Project.create(projectSeeds);
 
     console.log('all done!');
     process.exit(0);
