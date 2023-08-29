89ebb91a17e7ddadaf1afc163087481f036790ee
[Ghias Naser] <[ghiasnaser@gmail.com]>
2023-08-28 21:06:16 -0700

    adding the Font mutations and seed

--- server/seed/seed.js
+++ server/seed/seed.js
@@ -5,6 +5,8 @@ const Color = require('../models/Colors');
 const colorSeeds = require('./colorSeeds.json');
 const Gradient =require('../models/Gradients');
 const gradientSeeds = require('./gradientsSeeds.json');
+const Fonts =require('../models/Fonts');
+const fontSeeds = require('./fontSeeds.json');
 
 db.once('open', async () => {
   try {
@@ -14,6 +16,9 @@ db.once('open', async () => {
     await Color.create(colorSeeds);
     await Gradient.deleteMany({});
     await Gradient.create(gradientSeeds);
+    await Fonts.deleteMany({});
+    await Fonts.create(fontSeeds);
+
 
     console.log('all done!');
     process.exit(0);
