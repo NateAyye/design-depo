bdae1759c507ecb5001ebe61412e2f2510aa66e9
Beatriz Sandoval <beasaddress@gmail.com>
2023-08-28 19:59:26 -0700

    adding queries to queries.js

--- client/src/lib/queries.js
+++ client/src/lib/queries.js
@@ -7,3 +7,36 @@ query Query($colorId: ID!) {
     }
   }
 `;
+
+export const QUERY_ALL_COLORS = gql`
+query Query {
+    Colors {
+      _id
+      createdAt
+      hexCode
+      references
+    }
+  }
+`;
+
+export const QUERY_GRADIENT = gql`
+query Gradient($gradientId: ID!) {
+    Gradient(id: $gradientId) {
+      _id
+      createdAt
+      hexCode
+      references
+    }
+  }
+`;
+
+export const QUERY_ALL_GRADIENTS = gql`
+query Gradients {
+    Gradients {
+      _id
+      createdAt
+      hexCode
+      references
+    }
+  }
+`;
